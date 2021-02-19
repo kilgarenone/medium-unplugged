aa4b526f13ef1bed69c0b19e5e046c8917c20647
Kheoh Yee Wei <oldjoy@protonmail.com>
2021-02-19 20:38:08 +0800

    css

--- background.js
+++ background.js
@@ -231,8 +231,9 @@ extensionApi.webRequest.onBeforeRequest.addListener(
 
       const page = `<html>
                     <head>
-                      <meta charset="utf-8">
+                      <meta charset="UTF-8">
                       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
                     </head>
                     <body>${article.outerHTML}</body>
                     </html>`;
